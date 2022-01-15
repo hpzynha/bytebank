d2e80e5de16f6e7935e22ae7b857f1f7c0cff65c
Larissa <larissa.nogueira.rocha@gmail.com>
2022-01-15 14:55:12 -0400

    first commit

---
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nova-transferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.scss'],
+})
+export class NovaTransferenciaComponent {
+
+  valor!: number;
+  destino!: number;
+
+
+  transferir() {
+    console.log('Solicitada nova transferência');
+    console.log('Valor:', this.valor);
+    console.log('Destino: ', this.destino);
+
+  }
+}
